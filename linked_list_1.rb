83d42a98dbbec81abfbdc316f442ae3f9ae1c5a3
Ryne Vitek <rjvitek@hotmail.com>
2015-12-16 23:44:08 +0000

    LinkedList1 Finished

--- linked_list_1.rb
+++ linked_list_1.rb
@@ -8,19 +8,18 @@ class LinkedListNode
 end
 
 def print_values(list_node)
-  print "#{list_node.value} --> "
-  if list_node.next_node.nil?
-    print "nil\n"
-    return
-  else
-    print_values(list_node.next_node)
+  while list_node
+    x = list_node.pop
+    print "#{x.value} --> "
+    if x.next_node.nil?
+      print "nil\n"
+      return
+    end
   end
-
-
 end
 
 class Stack
-  attr_accessor :data, :list
+  attr_reader :data, :value, :next_node
 
   def initialize
     @data = nil
@@ -37,54 +36,26 @@ class Stack
     list_top
   end
 
-  def self.reverse_list(list)
-    new_list = Stack.new
-
-    while list
-      list_top = list.value
-      new_list.push(list_top)
-      list = list.next_node
-    end
-    
-    new_list
-  end
 
 end
-=begin
+
 def reverse_list(list)
-  new_list = Stack.new
+  new_stack = Stack.new
+  x = list.pop
 
-  while list
-    list_top = list.value
-    new_list.push(list_top)
-    list = list.next_node
+  while x
+    new_stack.push(x.value)
+    x = x.next_node
   end
     
-  new_list
+  new_stack
 end
-=end
-
 
 node1 = Stack.new
 node1.push(14)
 node1.push(15)
 node1.push(16)
+node1.push(17)
 
-x = reverse_list(node1)
-y = x.pop
-puts y.value
-
-
-
-
-=begin
-node3 = LinkedListNode.new(37)
-node2 = LinkedListNode.new(99, node3)
-node1 = LinkedListNode.new(12, node2)
-
-print_values(node1)
-
-node3.push(23)
-
-print_values(node1)
-=end
+fudge = reverse_list(node1)
+print_values(fudge)
